17aaf00f43ab6015415a0859a674a14534189957
awa411 <ad1117wi@gmail.com>
2021-03-10 17:56:39 +0900

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name,       null: false
-  validates :profile,    null: false
-  validates :occupation, null: false
-  validates :position,   null: false
+  validates :name,       presence: true
+  validates :profile,    presence: true
+  validates :occupation, presence: true
+  validates :position,   presence: true
   
   has_many :prototypes
   has_many :comments
